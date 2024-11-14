671fade37ead73b6b12432dfd21ffa92b0c2b831
Bang210 <bangvelope@gmail.com>
2024-11-14 17:39:14 +0900

    feat: keyword extraction

--- src/main/java/com/example/keyword/controller/KeywordController.java
+++ src/main/java/com/example/keyword/controller/KeywordController.java
@@ -1,10 +1,24 @@
 package com.example.keyword.controller;
 
+import com.example.keyword.client.CrawlingClient;
+import com.example.keyword.dto.CrawlingResponseDto;
+import com.example.keyword.service.KeywordService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class KeywordController {
 
+    private final CrawlingClient crawlingClient;
+    private final KeywordService keywordService;
+
+    @GetMapping("/receiverecent")
+    public void receiveRecentData() {
+        CrawlingResponseDto crawlingResponseDto = crawlingClient.receiveRecentData().getBody();
+        keywordService.extractKeyword(crawlingResponseDto);
+    }
 }
--- src/main/java/com/example/keyword/dto/CrawlingResponseDto.java
+++ src/main/java/com/example/keyword/dto/CrawlingResponseDto.java
@@ -1,16 +1,18 @@
 package com.example.keyword.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 @Getter
-@SuperBuilder(toBuilder = true)
+@Setter
 public class CrawlingResponseDto {
 
     //crawling 서버와의 통신을 위해 동일한 dto 생성
+    private long id;
     private String rawData;
     private LocalDateTime createdTime;
-    private long id;
+
 }
