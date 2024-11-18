fdd3434584d835c53c6b31eae8fd64d97327bef0
Bang210 <bangvelope@gmail.com>
2024-11-18 15:32:48 +0900

    feat: routing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 
 
 const Home = () => {
-
+<div>Hello nesKit</div>
 
 };
 
--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from '../components/layouts/Footer';
+import Home from "../pages/Home";
 
 function Router() {
     return (
         <div className='flex flex-col min-h-screen'>
             <BrowserRouter>
-                <Rooutes>
-                    <Home />
-                </Rooutes>
+                <Routes>
+                    <Route path="" element={<Home />} />
+                </Routes>
                 <Footer />
             </BrowserRouter>
         </div>
