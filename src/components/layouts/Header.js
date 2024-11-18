f152cbc4d5d2155b87966ca8966214f7bb26ca4a
Bang210 <bangvelope@gmail.com>
2024-11-18 18:56:27 +0900

    feat: add Header

---
+++ src/components/layouts/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <div className="navbar bg-base-300">
+            <div className="navbar-start"></div>
+            <div className="navbar-center">
+                <a href="" role="button" className="btn btn-ghost btn-wide text-xl flex justify-center">News Kit</a>
+            </div>
+            <div className="navbar-end"></div>
+        </div>
+    );
+};
+
+export default Header;
--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from '../components/layouts/Footer';
+import Header from '../components/layouts/Header';
 import Home from "./Home";
 
 function Router() {
     return (
         <div className='min-h-screen'>
             <BrowserRouter>
+                <Header />
                 <Routes>
                     <Route path="" element={<Home />} />
                 </Routes>
