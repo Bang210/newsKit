c03bdbc167dc6a9a81062e24d6278c9631fa4588
Bang210 <bangvelope@gmail.com>
2024-11-26 17:39:02 +0900

    feat: create childKeyword

---
+++ src/main/java/com/example/keyword/entity/ChildKeyword.java
@@ -0,0 +1,36 @@
+package com.example.keyword.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@SuperBuilder(toBuilder = true)
+public class ChildKeyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+
+    @Column
+    private String motherKeyword;
+
+    @Column
+    @ElementCollection
+    private List<String> childKeywordList;
+
+    @CreatedDate
+    @Column
+    private LocalDateTime createdTime;
+
+}
