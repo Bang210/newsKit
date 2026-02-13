4a51cf9ff11e1cd34f2881767d3756ca3eaf778e
Bang210 <bangvelope@gmail.com>
2026-02-13 21:41:58 +0900

    refactor:api url

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,6 +1,6 @@
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faHashtag } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faHashtag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
   return (
@@ -8,10 +8,14 @@ const Footer = () => {
       <div className="flex flex-row justify-between w-full">
         <div className="flex flex-row items-center justify-center">
           <FontAwesomeIcon icon={faHashtag} className="mr-3" />
-          <p>Copyright © 2024 - All right reserved</p>
+          <p>Copyright © 2026 - All right reserved</p>
         </div>
         <div className="items-center">
-          <a href="https://github.com/Bang210/newsKit" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/Bang210/newsKit"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faGithub} className="fa-xl" />
           </a>
         </div>
